605e3d929e076b610d0f6cd9ac51d3da1f650392
whiteblueblack <zh13@Mac-29.local>
2016-03-14 20:54:14 +0800

    123
    123

---
+++ main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  20131105744(3.10.3)
+//
+//  Created by ZH13 on 16/3/10.
+//  Copyright © 2016年 ZH13. All rights reserved.
+//
+
+import Foundation
+var dict = ["name":"wodeshijie","age":"19"]
+dict ["fex"]="hahaha"
+
+print(dict)
+print(dict["name"])
+
+
